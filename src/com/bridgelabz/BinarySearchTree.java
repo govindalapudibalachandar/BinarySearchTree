c92520366eced463e78ea7160ca785eb01a98a09
balachandar <govindalapudibalachandar@gmail.com>
2021-09-10 19:33:48 +0530

    Added:BinarySearchTree file is added

---
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -0,0 +1,54 @@
+package com.bridgelabz;
+
+public class BinarySearchTree {
+	
+    class Node
+    {
+        int key;
+        Node left, right;
+ 
+        public Node(int item)
+        {
+            key = item;
+            left = right = null;
+        }
+    }
+ 
+    // Root of BST
+    Node root;
+ 
+    // Constructor
+    BinarySearchTree()
+    {
+         root = null;
+    }
+ 
+    // This method mainly calls insertRec()
+    void insert(int key)
+    {
+         root = insertRec(root, key);
+    }
+ 
+    /* A recursive function to
+       insert a new key in BST */
+    Node insertRec(Node root, int key)
+    {
+ 
+        /* If the tree is empty,
+           return a new node */
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
+ 
+        /* Otherwise, recur down the tree */
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+        
+        return root;
+      }
+   
+}
